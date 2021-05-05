0d8fb4e11793742407fceb3fd16998b6fff2d055
Thomas Vo <thomasttvo@gmail.com>
2021-05-05 03:01:44 -0400

    added title

--- src/App.js
+++ src/App.js
@@ -7,13 +7,17 @@ import { Experience } from '@components/sections/Experience'
 import { Contact } from '@components/sections/Contact'
 import { Skills } from '@components/sections/Skills'
 import { data } from './data'
+import Helmet from 'react-helmet'
 
 export default function App() {
   return (
     <AppContainer>
+      <Helmet>
+        <title>{data.title}</title>
+      </Helmet>
       <PageTitle>{data.title}</PageTitle>
       <PageSubTitle>{data.subtitle}</PageSubTitle>
-      <Contact data={data.sections.contact} />
+      {/*<Contact data={data.sections.contact} />*/}
       <Summary data={data.sections.summary} />
       <Experience data={data.sections.experience} />
       <Skills data={data.sections.skills} />
